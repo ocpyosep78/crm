6fb280f112e68aacf021160758e03cefe4894a20
Diego Barreiro <diego.bindart@gmail.com>
2011-04-06 09:26:44 -0300

    new initialize script

---
+++ core/initialize.php
@@ -0,0 +1,16 @@
+<?php
+
+	# Set core path
+	
+	# Load app-specific constants
+	
+	# Load app-specific config
+	
+	# Load core config + app-specific defaults where unset
+	
+	# Load core constants + app-specific defaults where unset
+	
+	# Initialize Builder object and shortcuts to core libraries
+
+
+?>
