5ba99c079228eb4685e84d627b5776ee3b23a44c
Faliure <cuisdy@gmail.com>
2011-09-01 08:57:48 -0300

    phone length goes from 20 to 40

--- core/private/lib/Snippet/defs/customers.def.php
+++ core/private/lib/Snippet/defs/customers.def.php
@@ -97,7 +97,7 @@
 				'customer'		=> array('text', 2, 80),
 				'legal_name'	=> array('text', 2, 80),
 				'rut'			=> array('rut', NULL, 12),
-				'phone'			=> array('phone', 3, 20 ),
+				'phone'			=> array('phone', 3, 40 ),
 				'email'			=> array('email', NULL, 50),
 				'address'		=> array('text', NULL, 50),
 				'id_location'	=> array('selection'),
