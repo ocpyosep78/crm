df420668b2a73bee6f3bd4a3a43ccfa7cb3512f4
Diego Barreiro <diego.barreiro@internetbrands.com>
2012-11-13 02:50:36 -0200

    SnippetLayer_mysql auto-quering methods revised

--- core/private/lib/Snippet/layers/mysql.layer.php
+++ core/private/lib/Snippet/layers/mysql.layer.php
@@ -76,27 +76,23 @@ class SnippetLayer_mysql extends Connection
 		$fieldsSql = $this->getFieldsSql();
 		$tablesSql = $this->getTablesSql();
 
-		$sql = "SELECT {$fieldsSql}\n" .
-               "FROM {$tablesSql}";
+		$sql = "SELECT {$fieldsSql}\nFROM {$tablesSql}";
 
 		return $sql;
 	}
 
 	private function getFieldsSql()
 	{
-		$structure = $this->getStructure();
-		$columns = $structure['columns'];
+		extract($this->getStructure());
 
-		$tables = array_flip($structure['tables']['all']);
-		$fields = array_merge($columns['main'], $columns['to']);
+		$fields = array_merge($columns['own'], $columns['src']);
 
-		foreach ($fields as $field)
+		foreach ($fields as $fqn => $c)
 		{
-			$parts = explode('.', $field);
-			$name = $parts[2];
-			$t = $tables["{$parts[0]}.{$parts[1]}"];
+			$col = $c['COLUMN_NAME'];
+			$ord = $tables['ordinals']["{$c['TABLE_SCHEMA']}.{$c['TABLE_NAME']}"];
 
-			$fieldsSql[] = "`t{$t}`.`{$name}` AS '{$field}'";
+			$fieldsSql[] = "`t{$ord}`.`{$c['COLUMN_NAME']}` AS '{$fqn}'";
 		}
 
 		return empty($fieldsSql) ? array('*') : join(",\n       ", $fieldsSql);
@@ -104,23 +100,16 @@ class SnippetLayer_mysql extends Connection
 
 	private function getTablesSql()
 	{
-		$structure = $this->getStructure();
+		extract($this->getStructure());
 
-		$flipped = array_flip($structure['tables']['all']);
-
-		foreach ($structure['keys']['byCol'] as $col => $key)
+		foreach ($keys['src'] as $k)
 		{
-			if (in_array($col, $structure['columns']['to']))
-			{
-				$schema = $key['REFERENCED_TABLE_SCHEMA'];
-				$table = $key['REFERENCED_TABLE_NAME'];
-				$t = $flipped["{$schema}.{$table}"];
+			$ord = $tables['ordinals']["{$k['sch2']}.{$k['tbl2']}"];
 
-				$f1 = "`t0`.`{$key['COLUMN_NAME']}`";
-				$f2 = "`t{$t}`.`{$key['REFERENCED_COLUMN_NAME']}`";
+			$f1 = "`t0`.`{$k['col1']}`";
+			$f2 = "`t{$ord}`.`{$k['col2']}`";
 
-				$joins[] = "JOIN `{$table}` `t{$t}` ON ({$f1} = {$f2})";
-			}
+			$joins[] = "JOIN `{$k['sch2']}`.`{$k['tbl2']}` `t{$ord}` ON ({$f2} = {$f1})";
 		}
 
 		$sql = "`{$this->schema}`.`{$this->table}` `t0`\n" . join("\n", $joins);
@@ -134,84 +123,155 @@ class SnippetLayer_mysql extends Connection
 
 		if (!$structure)
 		{
-			$sql = "SELECT *
-			        FROM `information_schema`.`key_column_usage`
-			        WHERE (`TABLE_SCHEMA` = '{$this->params['db']}' AND `TABLE_NAME` = '{$this->table}')
-			           OR (`REFERENCED_TABLE_SCHEMA` = '{$this->params['db']}' AND `REFERENCED_TABLE_NAME` = '{$this->table}')";
-			$structure['keys']['all'] = $this->query($sql, 'array');
+			$keys    = $this->getKeys($this->schema, $this->table);
+			$tables  = $this->getTables($keys['all']);
+			$columns = $this->getColumns($tables['all']);
 
-			$types = array(1 => 'main', 2 => 'to', 3 => 'from');
-
-			// Make sure the main table goes first
-			$key1 = "{$this->schema}.{$this->table}";
-			$tables[$key1] = 1;
-			$tmpTables['all'][] = $key1;
-			$tmpTables[$types[1]][] = $key1;
-
-			foreach ($structure['keys']['all'] as $key)
-			{
-				$key2 = "{$key['REFERENCED_TABLE_SCHEMA']}.{$key['REFERENCED_TABLE_NAME']}";
-				if (!isset($tables[$key2]) && $key2 != '.')
-				{
-					$tables[$key2] = 2;
-					$tmpTables['all'][] = $key2;
-					$tmpTables[$types[2]][] = $key2;
-				}
-
-				$key3 = "{$key['TABLE_SCHEMA']}.{$key['TABLE_NAME']}";
-				if (!isset($tables[$key3]) && $key3 != '.')
-				{
-					$tables[$key3] = 3;
-					$tmpTables['all'][] = $key3;
-					$tmpTables[$types[3]][] = $key3;
-				}
-			}
-
-			foreach ($tmpTables as $type => $list)
-			{
-				$structure['tables'][$type] = array_values(array_unique($list));
-			}
-
-			// Prepare sql to filter query in search of all relevant columns
-			foreach ($structure['tables']['all'] as $key)
-			{
-				$info = explode('.', $key);
-				$filter[] = "(`TABLE_SCHEMA` = '{$info[0]}' AND `TABLE_NAME` = '{$info[1]}')";
-			}
-
-			$filterSql = empty($filter) ? 0 : join(' OR ', $filter);
-
-			// Get the structure and candidate columns of each listed table
-			$sql = "SELECT *
-			        FROM `information_schema`.`columns`
-			        WHERE (`TABLE_SCHEMA` = '{$this->schema}' AND `TABLE_NAME` = '{$this->table}')
-			        OR {$filterSql}";
-			$columns = $this->query($sql, 'array');
-
-			foreach ($columns as $column)
-			{
-				$schema = $column['TABLE_SCHEMA'];
-				$table = $column['TABLE_NAME'];
-				$field = $column['COLUMN_NAME'];
-
-				$type = $types[$tables["{$schema}.{$table}"]];
-
-				$structure['global'][$type][$schema][$table][$field] = $column;
-				$structure['columns']['all'][] = "{$schema}.{$table}.{$field}";
-				$structure['columns'][$type][] = "{$schema}.{$table}.{$field}";
-			}
-
-			foreach ($structure['keys']['all'] as &$key)
-			{
-				$c1 = "{$key['TABLE_SCHEMA']}.{$key['TABLE_NAME']}.{$key['COLUMN_NAME']}";
-				($c1 != '..') && ($structure['keys']['byCol'][$c1] =& $key);
-
-				$c2 = "{$key['REFERENCED_TABLE_SCHEMA']}.{$key['REFERENCED_TABLE_NAME']}.{$key['REFERENCED_COLUMN_NAME']}";
-				($c2 != '..') && ($structure['keys']['byCol'][$c2] =& $key);
-			}
+			$structure = compact('keys', 'tables', 'columns');
 		}
 
 		return $structure;
+	}
+
+	private function getKeys($schema, $table)
+	{
+		$all = $pri = $own = $src = $tgt = array();
+
+		$sql = "SELECT `CONSTRAINT_NAME` AS 'name',
+		               `TABLE_SCHEMA` AS 'sch1',
+		               `TABLE_NAME` AS 'tbl1',
+		               `COLUMN_NAME` AS 'col1',
+		               `REFERENCED_TABLE_SCHEMA` AS 'sch2',
+		               `REFERENCED_TABLE_NAME` AS 'tbl2',
+		               `REFERENCED_COLUMN_NAME` AS 'col2'
+		        FROM `information_schema`.`key_column_usage`
+		        WHERE (`TABLE_SCHEMA` = '{$schema}' AND `TABLE_NAME` = '{$table}')
+		           OR (`REFERENCED_TABLE_SCHEMA` = '{$schema}' AND `REFERENCED_TABLE_NAME` = '{$table}')";
+		$all = $this->query($sql, 'array');
+
+		foreach ($all as &$k)
+		{
+			if ($k['name'] == 'PRIMARY')
+			{
+				$k['type'] = 'pri';
+			}
+			elseif (!$k['sch2'])
+			{
+				$k['type'] = 'unique';
+			}
+			elseif (($k['sch1'] == $k['sch2']) && ($k['tbl1'] == $k['tbl2']))
+			{
+				$k['type'] = 'own';
+			}
+			elseif (($k['sch1'] == $schema) && ($k['tbl1'] == $table))
+			{
+				$k['type'] = 'src';
+			}
+			else
+			{
+				$k['type'] = 'tgt';
+			}
+
+			${$k['type']}[] =& $k;
+		}
+
+		$keys = compact('all', 'pri', 'own', 'src', 'tgt');
+
+		return $keys;
+	}
+
+	private function getTables($keys)
+	{
+		// Find out which tables are involved, to get their columns
+		$all = $own = $src = $tgt = array();
+
+		$ownKey = "{$this->schema}.{$this->table}";
+		$all[$ownKey] = array('schema'  => $this->schema,
+		                      'table'   => $this->table,
+		                      'type' => array('own' => 'own'));
+
+		$own[$ownKey] =& $all[$ownKey];
+
+		foreach ($keys as $k)
+		{
+			// Fully qualified name and entry skeleton structure
+			$fqn1 = "{$k['sch1']}.{$k['tbl1']}";
+			$fqn2 = "{$k['sch2']}.{$k['tbl2']}";
+
+			$t1 = array('schema' => $k['sch1'], 'table' => $k['tbl1'], 'type' => array());
+			$t2 = array('schema' => $k['sch2'], 'table' => $k['tbl2'], 'type' => array());
+
+			$k['tbl1'] && !isset($all[$fqn1]) && ($all[$fqn1] = $t1);
+			$k['tbl2'] && !isset($all[$fqn2]) && ($all[$fqn2] = $t2);
+
+			// Add the corresponding type to each table (src or tgt)
+			if ($k['tbl1'] && ($k['type'] == 'tgt'))
+			{
+				$src[$fqn1] =& $all[$fqn1];
+				$tgt[$fqn1]['type']['tgt'] = 'tgt';
+
+				// Flag main table as tgt as well
+				$tgt[$ownKey] =& $all[$ownKey];
+				$all[$ownKey]['type']['tgt'] = 'tgt';
+			}
+			elseif ($k['tbl2'] && ($k['type'] == 'src'))
+			{
+				$tgt[$fqn2] =& $all[$fqn2];
+				$tgt[$fqn2]['type']['src'] = 'src';
+
+				// Flag main table as src as well
+				$src[$ownKey] =& $all[$ownKey];
+				$all[$ownKey]['type']['src'] = 'src';
+			}
+		}
+
+		$ordinals = array_flip(array_keys($all));
+
+		$tables = compact('all', 'own', 'src', 'tgt', 'ordinals');
+
+		return $tables;
+	}
+
+	private function getColumns($tables)
+	{
+		// Prepare sql to filter query in search of all relevant columns
+		foreach ($tables as $t)
+		{
+			$cond[] = "(`TABLE_SCHEMA` = '{$t['schema']}' AND `TABLE_NAME` = '{$t['table']}')";
+		}
+
+		$condition = empty($cond) ? 0 : join(' OR ', $cond);
+
+		// Get the structure and candidate columns of each listed table
+		$sql = "SELECT *
+				FROM `information_schema`.`columns`
+				WHERE {$condition}";
+		$raw = $this->query($sql, 'array');
+
+		foreach ($raw as $c)
+		{
+			$schema = $c['TABLE_SCHEMA'];
+			$table = $c['TABLE_NAME'];
+			$field = $c['COLUMN_NAME'];
+
+			$tbl_fqn = "{$schema}.{$table}";
+			$col_fqn = "{$schema}.{$table}.{$field}";
+
+			$all[$col_fqn] = $c;
+
+			foreach (array('own', 'src', 'tgt') as $type)
+			{
+				if (in_array($type, $tables[$tbl_fqn]['type']))
+				{
+					$all[$col_fqn]['tbl_type'][$type] = $type;
+					${$type}[$col_fqn] =& $all[$col_fqn];
+				}
+			}
+		}
+
+		$columns = compact('all', 'own', 'src', 'tgt');
+
+		return $columns;
 	}
 
 }
