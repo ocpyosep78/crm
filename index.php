4ff96245d8927b01bdca62e90c49eeef5e2b103a
Diego Barreiro <diego.barreiro@internetbrands.com>
2013-02-16 00:38:17 -0200

    Minor changes

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
 <?php
 
-/***************************************************************************
+isset($_GET['chatCheck']) && die();
 
-	CRM_TEMPLATE: PHP Framework for building CRM-like applications
-	Copyright (C) 2011 Diego Barreiro <diego.bindart@gmail.com>
-	Licence: GNU GENERAL PUBLIC LICENSE <http://www.gnu.org/licenses/gpl.txt>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	
-***************************************************************************/
-
-
-	if( isset($_GET['chatCheck']) ) die();
-
-	require_once('app/index.php');
+require_once 'app/index.php';
